e41719fe74c09ab2ef137b699471d6be66d361b1
Guilherme Santos <32501503+Guilherme-SM@users.noreply.github.com>
2018-05-25 01:23:21 -0300

    Fixed the 'file not found' error redirect

--- Game_in_Sinatra/app.rb
+++ Game_in_Sinatra/app.rb
@@ -1,20 +1,32 @@
 # encoding: utf-8
 
 require 'sinatra'
+require 'find'
+
+def get_files(path)
+	files = []
+	Find.find(path) do |c|
+		files << File.basename(c, ".erb") if !File.directory?(c)
+	end
+	return
+end
 
 get '/' do
+	@views = get_files('./views')
 	"Servidor Sinatra Funcionando!"
 end
 
 get '/index' do
-	@pages = Pages.all
+	@views = get_files('./views')
 	erb :'pages/index.html.erb'
 end
 
 get '/cadastro' do
+	@views = get_files('./views')
 	erb :'pages/cadastro.html.erb'
 end
 
 get '/login' do
+	@views = get_files('./views')
 	erb :'pages/login.html.erb'
 end
